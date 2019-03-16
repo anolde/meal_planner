60117c3676c13851efb3501f47f047480bf99dbf
Raghu Betina <raghu@firstdraft.com>
2019-03-16 18:59:46 +0000

    Whitelist columns in Composition for ActiveAdmin

--- app/admin/compositions.rb
+++ app/admin/compositions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Composition do
+
+ permit_params :quantity, :recipe_id, :ingredient_id, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
