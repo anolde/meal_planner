b92beb6b7183a67104357c083845a07fb53e07bc
Raghu Betina <raghu@firstdraft.com>
2019-03-16 18:59:18 +0000

    Direct association between users and recipes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:recipes) }
+
     it { should have_many(:compositions) }
 
     it { should have_many(:restaurants) }
