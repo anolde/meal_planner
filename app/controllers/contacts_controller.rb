6e20bca3abb9c6243a038bb45a6d2e0910c197b3
Raghu Betina <raghu@firstdraft.com>
2019-03-16 18:59:57 +0000

    Only contact user can access show page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,5 @@
 class ContactsController < ApplicationController
-  before_action :current_user_must_be_contact_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_contact_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_contact_user
     contact = Contact.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
